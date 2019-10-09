c0a34f69c009ecad50f1309c0c0bafb399ec2c0d
Brianna DelValle <briannadelvalle@gmail.com>
2019-10-09 15:22:53 -0600

    Write post tests for /folders

--- app.test.js
+++ app.test.js
@@ -30,7 +30,7 @@ describe("Server", () => {
     })
   })
 
-  describe('GET /folders/:id', () =>{
+  describe('GET /folders/:id', () => {
     it('should return a 200 and a single folder if the folder exists', async () => {
       const expectedFolder = await database('folders').first();
       const id = expectedFolder.id;
@@ -42,5 +42,43 @@ describe("Server", () => {
       expect(folder.id).toEqual(expectedFolder.id)
       expect(folder.name).toEqual(expectedFolder.name)
     })
+
+    it('should return a 404 and the message "Folder not found" ', async () => {
+      const invalidId = -1;
+
+      const response = await request(app).get(`/api/v1/folders/${invalidId}`);
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toEqual(`Could not find folder with id ${invalidId}`);
+    })
   })
+
+  describe('POST /folders', () => {
+    
+    beforeEach(async () => {
+      await database.seed.run()
+    })
+
+    it('should post a new folder to the database and return the unique id', async () => {
+      const newFolder = { name: 'My New Favorite Test Palette'};
+
+      const res = await request(app).post('/api/v1/folders').send(newFolder);
+
+      const folders = await database('folders').where('id', res.body.id).select();
+      const folder = folders[0];
+
+      expect(res.status).toBe(201);
+      expect(folder.name).toEqual(newFolder.name);
+    })
+
+    it('should return a 422 and error when name parameter is missing', async () => {
+      const newFolder = {};
+
+      const res = await request(app).post('/api/v1/folders').send(newFolder);
+
+      expect(res.status).toBe(422);
+      expect(res.body.error).toEqual(`Expected format: { name: <String> }. You're missing a \"name\" property.`)
+    })
+  })
+
 });
