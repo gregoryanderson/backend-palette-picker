25a42f7bf1ee8b641f293d0f349e05a525f1d753
Brianna DelValle <briannadelvalle@gmail.com>
2019-10-09 15:11:42 -0600

    Write two GET endpoint tests for folders

--- app.test.js
+++ app.test.js
@@ -15,4 +15,32 @@ describe("Server", () => {
       expect(res.status).toBe(200);
     });
   });
+
+  describe("GET /folders", () => {
+    it('should return a 200 and all of the folders', async () => {
+      const expectedFolders = await database('folders').select();
+
+      const res = await request(app).get('/api/v1/folders');
+      const folders = res.body;
+
+      expect(res.status).toBe(200);
+      expect(folders[0].id).toEqual(expectedFolders[0].id)
+      expect(folders[0].name).toEqual(expectedFolders[0].name)
+
+    })
+  })
+
+  describe('GET /folders/:id', () =>{
+    it('should return a 200 and a single folder if the folder exists', async () => {
+      const expectedFolder = await database('folders').first();
+      const id = expectedFolder.id;
+
+      const res = await request(app).get(`/api/v1/folders/${id}`);
+      const folder = res.body[0];
+
+      expect(res.status).toBe(200);
+      expect(folder.id).toEqual(expectedFolder.id)
+      expect(folder.name).toEqual(expectedFolder.name)
+    })
+  })
 });
